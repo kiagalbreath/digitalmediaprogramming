de3be17ecdd8e4d0f8ee8247d010283d561a4aaa
kiagalbreath <keladagalbreath@gmail.com>
2019-09-15 15:43:16 -0500

    draft 1

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+var  fetchId= document.getElementById("menu");
+console.log(fetchId.childNodes.length);
+
+var fetchTags= document.getElementsByTagName("li");
+console.log("Number of li links: "+ fetchTags.length);
+
+
+
+
+
