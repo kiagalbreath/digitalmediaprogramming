71bb5b6acfe482ffbade9cf90c8fe8a7805a6dc0
kiagalbreath <keladagalbreath@gmail.com>
2019-09-15 15:59:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // JavaScript Document
 
 var  fetchId= document.getElementById("menu");
-console.log(fetchId.childNodes.length);
+console.log("Number of child nodes: " + fetchId.childNodes.length);
 
 var fetchTags= document.getElementsByTagName("li");
 console.log("Number of li links: "+ fetchTags.length);
+
+var newItem= document.createElement("li");
+
+var newText= document.createTextNode("New List Item");
+
+fetchId.appendChild(newItem);
+fetchId.appendChild(newText);
 
 
 
